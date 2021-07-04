975f4390356da36290d3009b58a87bee7379ea42
Naresh <pnareshsatya@gmail.com>
2021-07-04 16:25:38 +0530

    Equal to

--- LineComparisonTest.java
+++ LineComparisonTest.java
@@ -10,10 +10,18 @@ public class LineComparisonTest{
 		int y1 = sc1.nextInt();
 		System.out.println("Enter the y2: ");
 		int y2 = sc1.nextInt();
+                System.out.println("Enter the p1: ");
+                int p1 = sc1.nextInt();
+                System.out.println("Enter the p2: ");
+                int p2 = sc1.nextInt();
+                System.out.println("Enter the q1: ");
+                int q1 = sc1.nextInt();
+                System.out.println("Enter the q2: ");
+                int q2 = sc1.nextInt();
 
 		System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
-		LineComparison l = new LineComparison();
-		double result = l.length(x1,x2,y1,y2);
-		System.out.println("Length of 2 points is " + result);
+		LineComparisonEqual l= new LineComparisonEqual();
+		boolean result = l.isEqualTo(x1,x2,y1,y2,p1,p2,q1,q2);
+		System.out.println("Length of 2 points is" + result);
 	}
 }
